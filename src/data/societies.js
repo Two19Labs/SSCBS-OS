abc3ce9949d5f1eb2bb6a9d2d511571eec091b40
Two19Labs <two19labs@gmail.com>
2026-08-28 13:51:15 +0530

    feat(societies): add recruitment form schedule for FinX SSCBS

--- src/data/societies.js
+++ src/data/societies.js
@@ -502,9 +502,29 @@ export const DEMO_SOCIETIES = [
     categories: ['finance', 'consulting'],
     categoryLabels: ['Finance & Accounting', 'Consulting & Analytics'],
     description: 'Premier finance society running student investment funds and FinWiz. Recruitments will start soon, forms and info will come here soon!',
-    recruitmentFormUrl: null,
-    deadline: null,
-    statusText: 'Recruitments will start soon, forms and info will come here soon!',
+    scheduledForm: {
+      liveFrom: '2026-08-28T17:00:00+05:30',
+      recruitmentFormUrl: 'https://forms.gle/2xjVngeJpyEFDpEG9',
+      deadline: '2026-08-29T11:00:00+05:30',
+    },
+    get recruitmentFormUrl() {
+      if (this.scheduledForm && new Date() >= new Date(this.scheduledForm.liveFrom)) {
+        return this.scheduledForm.recruitmentFormUrl;
+      }
+      return null;
+    },
+    get deadline() {
+      if (this.scheduledForm && new Date() >= new Date(this.scheduledForm.liveFrom)) {
+        return this.scheduledForm.deadline;
+      }
+      return null;
+    },
+    get statusText() {
+      if (this.scheduledForm && new Date() >= new Date(this.scheduledForm.liveFrom)) {
+        return null;
+      }
+      return 'Recruitments will start soon, forms and info will come here soon!';
+    },
     officialPageUrl: OFFICIAL_COLLEGE_SOCIETIES_URL,
     instagramVideoUrl: 'https://www.instagram.com/reel/DbYWiMCh784/?igsh=MXJyd2VjamVrMWJxOA==',
     linkedinUrl: 'https://www.linkedin.com/company/finx---the-finance-society-of-sscbs',
