bc0de039cdf2bb3bdbac62f46396cb0786a8285e
Two19Labs <two19labs@gmail.com>
2026-08-29 18:50:13 +0530

    Add CDF recruitment form link, schedule and update POCs

--- src/data/societies.js
+++ src/data/societies.js
@@ -1379,8 +1379,8 @@ export const DEMO_SOCIETIES = [
     id: 'cdf-sscbs',
     pocs: [
       {
-        "name": "Khushi Chamola",
-        "phone": "9891464779"
+        "name": "Kanishka Kashyap",
+        "phone": "8178576756"
       },
       {
         "name": "Aryaman Gupta",
@@ -1394,9 +1394,38 @@ export const DEMO_SOCIETIES = [
     categories: ['ecell', 'wellness'],
     categoryLabels: ['Startups & Social Impact', 'Inclusion & Sports'],
     description: 'Empowering youth to achieve sustainable development goals and drive social entrepreneurship. Recruitments will start soon, forms and info will come here soon!',
-    recruitmentFormUrl: null,
-    deadline: null,
-    statusText: 'Recruitments will start soon, forms and info will come here soon!',
+    scheduledForm: {
+      liveFrom: '2026-08-29T19:00:00+05:30',
+      recruitmentFormUrl: 'https://forms.gle/htM6JBwBvxjV6E1U7',
+      initialDeadline: '2026-08-30T15:00:00+05:30',
+      extensionTrigger: '2026-08-30T14:55:00+05:30',
+      extendedDeadline: '2026-08-30T21:00:00+05:30',
+    },
+    get recruitmentFormUrl() {
+      if (this.scheduledForm && new Date() >= new Date(this.scheduledForm.liveFrom)) {
+        return this.scheduledForm.recruitmentFormUrl;
+      }
+      return null;
+    },
+    get deadline() {
+      if (!this.scheduledForm) return null;
+      const now = new Date();
+      if (now < new Date(this.scheduledForm.liveFrom)) {
+        return null;
+      }
+      if (this.scheduledForm.extensionTrigger && now >= new Date(this.scheduledForm.extensionTrigger)) {
+        return this.scheduledForm.extendedDeadline;
+      }
+      return this.scheduledForm.initialDeadline;
+    },
+    get statusText() {
+      if (!this.scheduledForm) return 'Recruitments will start soon, forms and info will come here soon!';
+      const now = new Date();
+      if (now < new Date(this.scheduledForm.liveFrom)) {
+        return 'Recruitments will start soon, forms and info will come here soon!';
+      }
+      return null;
+    },
     officialPageUrl: OFFICIAL_COLLEGE_SOCIETIES_URL,
     instagramVideoUrl: 'https://www.instagram.com/reel/DbVX0gwJfQ6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==&igsi=MzRlODBiNWFlZA==',
     linkedinUrl: 'https://www.linkedin.com/company/connecting-dreams-foundation-sscbs/',
