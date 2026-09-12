c7eb7d17d34dbd4a2a4537369fd47fb4e59e111c
Two19Labs <two19labs@gmail.com>
2026-09-12 12:33:33 +0530

    Remove UNSTOP ONLY badge from main page title header

--- src/components/CaseCompsPage.jsx
+++ src/components/CaseCompsPage.jsx
@@ -442,7 +442,6 @@ export default function CaseCompsPage({ onBack, onNavigate }) {
           <div>
             <div className="cc-title-row">
               <h1 className="cc-title">Case Competitions</h1>
-              <span className="cc-unstop-tag">UNSTOP ONLY</span>
             </div>
             <p className="cc-subtitle">
               It's competitions season! Find opportunities relevant to CBS folks right here, synced with and pulled from Unstop, all filterable! :)
